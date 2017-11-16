897309bc76c9bcfbde0aaeadadb00a4421ade369
egoeast <das.east@mail.ru>
2017-11-16 20:07:31 +0300

    trans

--- controllers/TranslationController.php
+++ controllers/TranslationController.php
@@ -2,31 +2,31 @@
 
 namespace app\controllers;
 
-use Google\Cloud\Translate\TranslateClient;
+//use Google\Cloud\Translate\TranslateClient;
+use yii\console\Controller;
+use app\models\WeatherStation;
 
-class TranslationController extends \yii\web\Controller
+class TranslationController extends Controller
 {
-    public function actionIndex()
+    public function yandexTranslate($text, $lang)
     {
-        $text = 'motherfucker';
-        $lang = 'ru';
-        //$translate = new TranslateClient();
-        //$result = $translate->translate($text, [
-        //    'target' => $lang,
-        //]);
-        $params = array( 'key' => 'trnsl.1.1.20171116T103815Z.de890509a05594eb.07ad8f63c2e9da3b843adc43104ebcde9bb0e6d4
-', 'text' => $text, 'lang' => 'en-ru',);
+        $apiKey = 'trnsl.1.1.20171116T103815Z.de890509a05594eb.07ad8f63c2e9da3b843adc43104ebcde9bb0e6d4';
+        $params = array( 'key' => $apiKey, 'text' => $text, 'lang' => $lang,);
         $query = http_build_query($params);
         $response = file_get_contents('https://translate.yandex.net/api/v1.5/tr.json/translate?'.$query);
-
-        echo $query;
-
         $data = json_decode($response, true);
         $text = $data['text'][0];
-        echo $text;
+        return $text;
+    }
 
-        //echo $result;
-        //return $this->render('index');
+    public function actionIndex()
+    {
+        $text = 'ProTip! Updating your profile with your name, location, and a profile picture helps other GitHub users get to know you.';
+        $lang = 'en-ru';
+        $result = $this->yandexTranslate($text, $lang);
+
+        $model = new WeatherStation();
+        return $this->render('index.twig', ['result' => $result, 'model' => $model]);
     }
 
 }
